53153edf0282dec6bc7f6e91e01561044e953c06
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-03 02:43:53 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,16 +15,20 @@ html = File.read('fixtures/kickstarter.html')
 kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
+
   html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html) #converting html to 'NodeSet' that can be manipulated
   
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value, :description => project.css("p.bbcard_blurb").text, :location => project.css("span.location-name").text, :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+    }
   end
+  
   projects
-  binding.pry
+  
 end
 
-create_project_hash
