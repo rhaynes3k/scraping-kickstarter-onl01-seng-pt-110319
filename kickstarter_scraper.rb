dadbb7567141386fadc6231db93092e80f85a6b2
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-03 02:13:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,7 @@
 
 # require libraries/modules here
 
-# projects: kickstarter.css("li.project.grid_4")
+# projects: kickstarter.css("li.project.grid_4").first
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
